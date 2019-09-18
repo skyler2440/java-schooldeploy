430bed85b96c409efee2ed9ddc26815e424ceb61
skyler dowdy <skylerwebdev@gmail.com>
2019-09-18 10:39:25 -0400

    Added Config Files Need to update

---
+++ deploy/src/main/java/local/skylerwebdev/javaschooldeploy/JavaschooldeployApplication.java
@@ -0,0 +1,15 @@
+package local.skylerwebdev.javaschooldeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaschooldeployApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavaschooldeployApplication.class, args);
+    }
+
+}
---
+++ deploy/src/test/java/local/skylerwebdev/javaschooldeploy/JavaschooldeployApplicationTests.java
@@ -0,0 +1,18 @@
+package local.skylerwebdev.javaschooldeploy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaschooldeployApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
